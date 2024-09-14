6854a5e55c861f4f2d154934081ba51626bed81b
akashkrisha <akash@krishaweb.com>
2024-09-14 14:27:14 +0530

    header running

--- include/header.php
+++ include/header.php
@@ -7,43 +7,41 @@
             </div>
         </div>
         <div class="header-bottom">
-            <div class="container">
-                <div class="head-logo">
-                    <a href="#">
-                        <img src="assets/images/logo.png" alt="header logo" width="265" height="142">
-                    </a>
+            <div class="head-logo">
+                <a href="#">
+                    <img src="assets/images/logo.png" alt="header logo" width="265" height="142">
+                </a>
+            </div>
+            <div class="header-menu">
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">FAQs</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                        <li>
+                            <a href="#">Blog</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="header-menu">
-                    <div class="menu">
-                        <ul>
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="#">About</a>
-                            </li>
-                            <li>
-                                <a href="#">Services</a>
-                            </li>
-                            <li>
-                                <a href="#">FAQs</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact</a>
-                            </li>
-                            <li>
-                                <a href="#">Blog</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="head-contact">
-                        <ul>
-                            <li><a href="mailto:info@lightitupevents.com">info@lightitupevents.com</a></li>
-                            <li><a href="tel:info@321-438-6314">321-438-6314</a></li>
-                        </ul>
-                        <div class="btn-wrap">
-                            <a href="#" class="btn">Call to Action</a>
-                        </div>
+                <div class="head-contact">
+                    <ul>
+                        <li><a href="mailto:info@lightitupevents.com">info@lightitupevents.com</a></li>
+                        <li><a href="tel:info@321-438-6314">321-438-6314</a></li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn">Call to Action</a>
                     </div>
                 </div>
             </div>
