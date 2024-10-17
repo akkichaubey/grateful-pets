ae5266bcb1de404ea8fe05fca1cd8c22c5b8b8a3
akashkrisha <akash@krishaweb.com>
2024-10-17 21:42:28 +0530

    About page

---
+++ about-us.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include ("include/head.php"); ?>
+</head>
+
+<body>
+    <!-- header section start -->
+    <?php include ("include/header.php"); ?>
+    <!-- header section end -->
+
+    <!-- hero banner start -->
+    <div class="banner hero-banner">
+        <div class="container">
+            <div class="column-wrap">
+                <div class="col-left">
+                    <div class="banner-info text-md">
+                        <h1 class="h1 banner-title" data-gsap="fadeUp">About us</h1>
+                        <div class="text-wrap" data-gsap="fadeUp" data-delay="0.2">
+                            <p>Netus eleifend consequat fermentum dapibus tempor. Placerat class varius nam risus
+                                finibus. Ex penatibus condimentum nullam nostra phasellus nisi pulvinar.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-right">
+                    <div class="banner-img" data-gsap="fadeUp" data-delay="0.2">
+                        <img src="./assets/images/photo-no-background.webp" alt="img" width="830" height="849">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- hero banner end -->
+
+    <!-- home about end -->
+    <div class="home-about pt-180 pb-150">
+        <div class="inner-section">
+            <div class="container">
+                <div class="row align-items-center row-gap-30">
+                    <div class="col-md-6">
+                        <div class="video-wrap">
+                            <div class="video-box" data-gsap="fadeUp">
+                                <img src="./assets/images/video-thumb.webp" alt="image" width="721" height="440">
+                                <div class="video-btn">
+                                    <a href="https://youtu.be/D0UnqGm_miA" data-fancybox>
+                                        <svg class="icon" width="25" height="29">
+                                            <use xlink:href="./assets/images/svgsprit.svg#play-icon"></use>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="content-box text-sm">
+                            <div class="title-wrap" data-gsap="fadeUp">
+                                <h2 class="h2">About Us</h2>
+                            </div>
+                            <div class="text-wrap" data-gsap="fadeUp" data-delay="0.2">
+                                <p>Welcome to Grateful Pets, where your pets are treated like family. Located in the
+                                    heart of Soulard, just minutes from Lafayette Square and Downtown St. Louis, we
+                                    offer daycare in our state-of-the-art facility, professional dog training,
+                                    all-inclusive spa services, and CPR classes. Our experienced team is passionate
+                                    about providing the best care for your furry friends, ensuring they receive the
+                                    attention and love they deserve.</p>
+                            </div>
+                            <div class="btn-wrap" data-gsap="fadeUp" data-delay="0.3">
+                                <a href="#" class="btn">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="inner-section">
+            <div class="container">
+                <div class="row align-items-center row-gap-30">
+                    <div class="col-md-6">
+                        <div class="video-wrap">
+                            <div class="video-box" data-gsap="fadeUp">
+                                <img src="./assets/images/about-1.webp" alt="image" width="721" height="440">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="content-box text-sm">
+                            <div class="title-wrap" data-gsap="fadeUp">
+                                <h2 class="h2">Our Mission</h2>
+                            </div>
+                            <div class="text-wrap" data-gsap="fadeUp" data-delay="0.2">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel justo nec mi
+                                    ullamcorper efficitur. Donec lacinia, turpis a elementum scelerisque, dui risus
+                                    sodales justo, vitae semper odio lorem a nisl.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- home about end -->
+
+    <!-- about our value start -->
+    <div class="about-value pb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="value-box title-box" data-gsap="fadeUp">
+                        <h2 class="h2">Our <br>Values</h2>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="value-box text-sm" data-gsap="fadeUp">
+                        <h3 class="h4">Innovation</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor felis eget turpis
+                            sollicitudin, vel tincidunt odio efficitur.</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="value-box text-sm" data-gsap="fadeUp">
+                        <h3 class="h4">Quality</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor felis eget turpis
+                            sollicitudin, vel tincidunt odio efficitur.</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="value-box text-sm" data-gsap="fadeUp">
+                        <h3 class="h4">Sustainability</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor felis eget turpis
+                            sollicitudin, vel tincidunt odio efficitur.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- about our value end -->
+    <!-- why choose us start -->
+    <div class="about-choose pb-150">
+        <div class="container">
+            <div class="title-wrap text-center">
+                <h2 class="h2" data-gsap="fadeUp">Why Choose Us</h2>
+                <div class="sub-text text-sm" data-gsap="fadeUp">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel justo nec mi ullamcorper
+                        efficitur. Donec lacinia, turpis a elementum scelerisque, dui risus sodales justo, vitae semper
+                        odio lorem a nisl.</p>
+                </div>
+            </div>
+            <div class="choose-list">
+                <ul>
+                    <li>
+                        <div class="about-choose-box text-sm" data-gsap="fadeUp">
+                            <div class="icon-box"></div>
+                            <h3 class="h4">Expert Groomers & Trainers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Each of our products is designed
+                                and crafted with the utmost attention to detail, ensuring durability, functionality, and
+                                style. Whether it's materials or assembly, we never compromise on quality.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="about-choose-box text-sm" data-gsap="fadeUp">
+                            <div class="icon-box"></div>
+                            <h3 class="h4">Expert Groomers & Trainers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Each of our products is designed
+                                and crafted with the utmost attention to detail, ensuring durability, functionality, and
+                                style. Whether it's materials or assembly, we never compromise on quality.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="about-choose-box text-sm" data-gsap="fadeUp">
+                            <div class="icon-box"></div>
+                            <h3 class="h4">Expert Groomers & Trainers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Each of our products is designed
+                                and crafted with the utmost attention to detail, ensuring durability, functionality, and
+                                style. Whether it's materials or assembly, we never compromise on quality.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="about-choose-box text-sm" data-gsap="fadeUp">
+                            <div class="icon-box"></div>
+                            <h3 class="h4">Expert Groomers & Trainers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Each of our products is designed
+                                and crafted with the utmost attention to detail, ensuring durability, functionality, and
+                                style. Whether it's materials or assembly, we never compromise on quality.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="about-choose-box text-sm" data-gsap="fadeUp">
+                            <div class="icon-box"></div>
+                            <h3 class="h4">Expert Groomers & Trainers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Each of our products is designed
+                                and crafted with the utmost attention to detail, ensuring durability, functionality, and
+                                style. Whether it's materials or assembly, we never compromise on quality.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="about-choose-box text-sm" data-gsap="fadeUp">
+                            <div class="icon-box"></div>
+                            <h3 class="h4">Expert Groomers & Trainers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Each of our products is designed
+                                and crafted with the utmost attention to detail, ensuring durability, functionality, and
+                                style. Whether it's materials or assembly, we never compromise on quality.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- why choose us end -->
+
+    <!-- banner-cta start -->
+    <div class="banner-cta">
+        <div class="container">
+            <h2 class="h2" data-gsap="fadeUp">Ready to make your pet, a grateful pet?<br> Schedule a Free Tour!</h2>
+            <div class="btn-wrap" data-gsap="fadeUp">
+                <a href="#" class="btn btn-white">Pick a Good Time</a>
+            </div>
+        </div>
+    </div>
+    <!-- banner-cta end -->
+
+    <!-- footer section start -->
+    <?php include ("include/footer.php"); ?>
+    <?php include ("include/script.php"); ?>
+    <!-- footer section end -->
+</body>
+
+</html>
